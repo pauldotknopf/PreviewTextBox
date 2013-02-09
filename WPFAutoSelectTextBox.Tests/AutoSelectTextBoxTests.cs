c9bdd664b5f5030b091ebfe85d46d438eee76414
Paul Knopf <theonlylawislove@gmail.com>
2013-02-09 15:26:06 -0500

    Working

---
+++ WPFAutoSelectTextBox.Tests/AutoSelectTextBoxTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using NUnit.Framework;
+using Application = System.Windows.Application;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+
+namespace WPFAutoSelectTextBox.Tests
+{
+    [TestFixture]
+    public class AutoSelectTextBoxTests
+    {
+        private Application _application;
+        private Window _window;
+
+        [Test]
+        public void Can_preview_text()
+        {
+            var tb = BuildTextBox();
+            tb.AutoSelectText = "test";
+            RaiseKeyPressed(tb, Key.T);
+            RaiseKeyPressed(tb, Key.L);
+            Assert.AreEqual("l", tb.Text);
+        }
+
+        private void RaiseKeyPressed(AutoSelectTextBox textBox, Key key)
+        {
+            textBox.Focus();
+            SendMessage(new WindowInteropHelper(_window).Handle, 0x0102, MapVirtualKey((int)key, MAPVK_VK_TO_CHAR), 0);
+        }
+
+        private AutoSelectTextBox BuildTextBox()
+        {
+            _window = new Window();
+            
+            var stackPanel = new StackPanel();
+            var textBox = new AutoSelectTextBox();
+            stackPanel.Children.Add(textBox);
+
+            _window.Content = stackPanel;
+            _window.Show();
+            textBox.Focus();
+            return textBox;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+
+        const uint MAPVK_VK_TO_CHAR = 0x02;
+        [DllImport("user32.dll")]
+        static extern int MapVirtualKey(int uCode, uint uMapType);
+    }
+}
---
+++ WPFAutoSelectTextBox/AutoSelectTextBox.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFAutoSelectTextBox
+{
+    public class AutoSelectTextBox : TextBox
+    {
+        private string _realText = string.Empty;
+        private Key _lastKey;
+        private bool _ignoreNextChanged;
+
+        public AutoSelectTextBox()
+        {
+            this.TextChanged += OnTextChanged;
+            this.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs keyEventArgs)
+        {
+            _lastKey = keyEventArgs.Key;
+            if(_lastKey == Key.Back)
+            {
+                _ignoreNextChanged = true;
+            }else if (_lastKey == Key.Enter)
+            {
+                // we want to select the preview text
+                if (!string.IsNullOrEmpty(AutoSelectText))
+                {
+                    Text = AutoSelectText;
+                    SelectionStart = AutoSelectText.Length;
+                }
+            }
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            if(!string.IsNullOrEmpty(AutoSelectText))
+                Text = _realText;
+        }
+
+        public static readonly DependencyProperty AutoSelectTextProperty =
+            DependencyProperty.Register("AutoSelectText", typeof (string), typeof (AutoSelectTextBox), new PropertyMetadata(default(string)));
+
+        public string AutoSelectText
+        {
+            get { return (string) GetValue(AutoSelectTextProperty); }
+            set { SetValue(AutoSelectTextProperty, value); }
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs textChangedEventArgs)
+        {
+            if(_ignoreNextChanged)
+            {
+                Debug.WriteLine("OnTextChanged: Ignoring");
+                _ignoreNextChanged = !_ignoreNextChanged;
+                return;
+            }else
+            {
+                Debug.WriteLine("OnTextChanged: Performing");
+            }
+            
+            _realText = Text;
+            Debug.WriteLine("OnTextChanged: Real text is '" + _realText + "'");
+
+            if(AutoSelectText.StartsWith(Text, StringComparison.InvariantCultureIgnoreCase))
+            {
+                Debug.WriteLine("OnTextChanged: The text starts with our auto select phrase. Change text to our auto select text and highlight the preview text");
+                // it matches
+                _ignoreNextChanged = true;
+                Text = AutoSelectText;
+                SelectionStart = _realText.Length;
+                SelectionLength = AutoSelectText.Length - _realText.Length;
+            }
+        }
+
+        private bool Contains(string source, string toCheck, StringComparison comp)
+        {
+            return source.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+}
